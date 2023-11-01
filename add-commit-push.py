296390dd70294896fb90e15eaa51af13d1eb90c0
MatthewSittler <matthewjsittler@lewisu.edu>
2023-11-01 10:28:22 -0500

    change file

---
+++ add-commit-push.py
@@ -0,0 +1,8 @@
+import os
+
+print("Add Commit Push")
+#print("\n git status")
+#os.system("git status")
+#os.system("git add -A")
+os.system("git commit -m 'Update files.'")
+#os.system("git push")
